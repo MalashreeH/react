e7265f8b8d2a419798e32205786bf145fad1fd6b
MalashreeH <127289640+MalashreeH@users.noreply.github.com>
2023-03-08 13:15:08 +0530

    Add files via upload

---
+++ Logo.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+ const Logo=()=>{
+    return(
+        <div>
+            <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.edigitalagency.com.au%2Flogos%2Fnew-netflix-logo-png%2F&psig=AOvVaw0Hgk-Kc0L1sHWaK-xQw5Li&ust=1678170455673000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCIjewqHWxv0CFQAAAAAdAAAAABAE" alt="" />
+              </div>   )
+ }
+ export default Logo
---
+++ Menu.jsx
@@ -0,0 +1,26 @@
+import React from "react";  
+
+ const Menu =()=>{
+    return(
+        <div className="menuBlock">
+            <ul>
+              <li>
+                <a href="">HOME</a>
+              </li>
+              <li>
+                <a href="">ABOUT</a>
+              </li>
+              <li>
+                <a href="">SERVICE</a>
+              </li>
+              <li>
+                <a href="">MOVIES</a>
+              </li>
+              <li>
+                <a href="">SIGNUP</a>
+              </li>
+            </ul>
+        </div>
+    )
+ }
+ export default Menu
