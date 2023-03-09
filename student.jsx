07077a00e97636193818850f40d7865960860c28
MalashreeH <127289640+MalashreeH@users.noreply.github.com>
2023-03-09 08:31:44 -0800

    Add files via upload

---
+++ student.jsx
@@ -0,0 +1,140 @@
+import React from "react";
+const Student=(props)=>
+{
+return(
+    <div className="parentBlock">
+        <div className="A">
+
+        <div className="mainBlock">
+            <img src={props.data[0].studentImg} alt="" />
+            <h1 className="h1">{props.data[0].studentId} </h1>
+            <h2 className="h2">{props.data[0].studentName}</h2>
+            <h3 className="h3">{props.data[0].branch}</h3>
+        </div>
+        <div className="mainBlock">
+            <img src={props.data[1].studentImg} alt="" />
+            <h1 className="h1">{props.data[1].studentId} </h1>
+            <h2 className="h2">{props.data[1].studentName}</h2>
+            <h3 className="h3">{props.data[1].branch}</h3>
+        </div>
+        <div className="mainBlock">
+            <img src={props.data[2].studentImg} alt="" />
+            <h1 className="h1">{props.data[2].studentId} </h1>
+            <h2 className="h2">{props.data[2].studentName}</h2>
+            <h3 className="h3">{props.data[2].branch}</h3>
+        </div>
+        <div className="mainBlock">
+            <img src={props.data[3].studentImg} alt="" />
+            <h1 className="h1">{props.data[3].studentId} </h1>
+            <h2 className="h2">{props.data[3].studentName}</h2>
+            <h3 className="h3">{props.data[3].branch}</h3>
+        </div>
+        <div className="mainBlock">
+            <img src={props.data[4].studentImg} alt="" />
+            <h1 className="h1">{props.data[4].studentId} </h1>
+            <h2 className="h2">{props.data[4].studentName}</h2>
+            <h3 className="h3">{props.data[4].branch}</h3>
+        </div>
+        </div>
+        <div className="A">
+        <div className="mainBlock">
+            <img src={props.data[5].studentImg} alt="" />
+            <h1 className="h1">{props.data[5].studentId} </h1>
+            <h2 className="h2">{props.data[5].studentName}</h2>
+            <h3 className="h3">{props.data[5].branch}</h3>
+        </div>
+        
+        
+        <div className="mainBlock">
+            <img src={props.data[6].studentImg} alt="" />
+            <h1 className="h1">{props.data[6].studentId} </h1>
+            <h2 className="h2">{props.data[6].studentName}</h2>
+            <h3 className="h3">{props.data[6].branch}</h3>
+        </div>
+        <div className="mainBlock">
+            <img src={props.data[7].studentImg} alt="" />
+            <h1 className="h1">{props.data[7].studentId} </h1>
+            <h2 className="h2">{props.data[7].studentName}</h2>
+            <h3 className="h3">{props.data[7].branch}</h3>
+        </div>
+        <div className="mainBlock">
+            <img src={props.data[8].studentImg} alt="" />
+            <h1 className="h1">{props.data[8].studentId} </h1>
+            <h2 className="h2">{props.data[8].studentName}</h2>
+            <h3 className="h3">{props.data[8].branch}</h3>
+        </div>
+        <div className="mainBlock">
+            <img src={props.data[9].studentImg} alt="" />
+            <h1 className="h1">{props.data[9].studentId} </h1>
+            <h2 className="h2">{props.data[9].studentName}</h2>
+            <h3 className="h3">{props.data[9].branch}</h3>
+        </div>
+        </div>
+        <div className="A">
+        <div className="mainBlock">
+            <img src={props.data[10].studentImg} alt="" />
+            <h1 className="h1">{props.data[10].studentId} </h1>
+            <h2 className="h2">{props.data[10].studentName}</h2>
+            <h3 className="h3">{props.data[10].branch}</h3>
+        </div>
+        <div className="mainBlock">
+            <img src={props.data[11].studentImg} alt="" />
+            <h1 className="h1">{props.data[11].studentId} </h1>
+            <h2  className="h2">{props.data[11].studentName}</h2>
+            <h3 className="h3">{props.data[11].branch}</h3>
+        </div>
+        <div className="mainBlock">
+            <img src={props.data[12].studentImg} alt="" />
+            <h1 className="h1">{props.data[12].studentId} </h1>
+            <h2 className="h2">{props.data[12].studentName}</h2>
+            <h3 className="h3">{props.data[12].branch}</h3>
+        </div>
+        <div className="mainBlock">
+            <img src={props.data[13].studentImg} alt="" />
+            <h1 className="h1">{props.data[13].studentId} </h1>
+            <h2 className="h2">{props.data[13].studentName}</h2>
+            <h3 className="h3">{props.data[13].branch}</h3>
+        </div>
+        <div className="mainBlock">
+            <img src={props.data[14].studentImg} alt="" />
+            <h1 className="h1">{props.data[14].studentId} </h1>
+            <h2 className="h2">{props.data[14].studentName}</h2>
+            <h3 className="h3">{props.data[14].branch}</h3>
+        </div>
+        </div>
+        <div className="A">
+        <div className="mainBlock">
+            <img src={props.data[15].studentImg} alt="" />
+            <h1 className="h1">{props.data[15].studentId} </h1>
+            <h2 className="h2">{props.data[15].studentName}</h2>
+            <h3 className="h3">{props.data[15].branch}</h3>
+        </div>
+        <div className="mainBlock">
+            <img src={props.data[16].studentImg} alt="" />
+            <h1 className="h1">{props.data[16].studentId} </h1>
+            <h2 className="h2">{props.data[16].studentName}</h2>
+            <h3 className="h3">{props.data[16].branch}</h3>
+        </div>
+        <div className="mainBlock">
+            <img src={props.data[17].studentImg} alt="" />
+            <h1 className="h1">{props.data[17].studentId} </h1>
+            <h2 className="h2">{props.data[17].studentName}</h2>
+            <h3 className="h3">{props.data[17].branch}</h3>
+        </div>
+        <div className="mainBlock">
+            <img src={props.data[18].studentImg} alt="" />
+            <h1 className="h1">{props.data[18].studentId} </h1>
+            <h2 className="h2">{props.data[18].studentName}</h2>
+            <h3 className="h3">{props.data[18].branch}</h3>
+        </div>
+        <div className="mainBlock">
+            <img src={props.data[19].studentImg} alt="" />
+            <h1 className="h1">{props.data[19].studentId} </h1>
+            <h2 className="h2">{props.data[19].studentName}</h2>
+            <h3 className="h3">{props.data[19].branch}</h3>
+        </div>
+        </div>
+    </div>
+)
+}
+export default Student
