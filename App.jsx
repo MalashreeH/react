7230b41a7b8ce11d1b814f9c03f84c6296837941
MalashreeH <127289640+MalashreeH@users.noreply.github.com>
2023-03-15 22:43:02 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,15 +1,45 @@
-import React from "react"; 
-import Student from "./student.jsx"
-import JSON from "./studentdata.json";
-import "./global.css"
-const App=()=>
-{
+import React, { Component } from 'react'
+import "./btn.css"
+
+export default class App extends Component {
+  constructor()
+  {
+    super()
+    this.state={
+      count:0
+    }
     
-    return(
-        <div>
-            
-        <Student data={JSON}/>
-        </div>
-    )
+  }
+handleIncrement=()=>{
+  this.setState({count:this.state.count+1})
+  
 }
-export default App
+handleDecrement=()=>{
+  this.setState({count:this.state.count-1})
+  
+}
+handleReset=function()
+{
+  this.setState({count:0})
+}
+
+
+  render() {
+    return (
+      
+      <div style={{backgroundColor:"yellow", height:"300px", width:"450px",border:"2px solid blue",justifyContent:"center" , marginTop:"50px",marginLeft:"200px",borderRadius:"200px"}}>
+      
+        <div  className="mainBlock" style={{  alignItems:"center" ,justifyContent:"center",display:"flex", flexDirection:"column", top:"20px", height:"50vh"}}>
+        <h1 style={{color:"red"}}>{this.state.count}</h1>
+       
+        <div className="btngroup" >
+        
+        <button onClick={this.handleIncrement}>increment </button>
+        <button onClick={this.handleDecrement}>decrement</button>
+        <button onClick={this.handleReset.bind(this)}>RESET</button>
+      </div></div>
+      </div>
+    )
+  }
+
+}
